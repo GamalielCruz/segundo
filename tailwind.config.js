6efea21361710db50a59a5e2afe8fae4a8f4e52b
GamalielCruz <128826239+GamalielCruz@users.noreply.github.com>
2023-04-01 00:11:40 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        danger: '#ff5f40',
+        info: {
+          900: '#234e52',
+          800: '#285e61',
+        }
+      },
+    },
+    fontFamily: {
+      rale: ['Raleway'],
+    },
+  },
+  variants: {},
+  plugins: [],
+}
